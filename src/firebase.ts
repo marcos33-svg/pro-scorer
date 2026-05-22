08677fa392f1b0bf3afad5f9519e647effa5b7a1
Meet z <k23469221@gmail.com>
2026-05-22 23:44:54 +0530

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,20 @@
+// src/firebase.ts
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCB6HpvGzvGT-2OUPsJmScL5qHNR3QkmYE",
+  authDomain: "pro-scorer-2a5ef.firebaseapp.com",
+  databaseURL: "https://pro-scorer-2a5ef-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pro-scorer-2a5ef",
+  storageBucket: "pro-scorer-2a5ef.firebasestorage.app",
+  messagingSenderId: "201783156427",
+  appId: "1:201783156427:web:10933b1bd40d054ff0854e",
+  measurementId: "G-DYNB9PX92F"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const analytics = getAnalytics(app);
+export default app;
